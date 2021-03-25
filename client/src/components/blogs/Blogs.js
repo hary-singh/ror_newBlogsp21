8e01a7bf7fede3eef3bf3c646c0a3431c5dda318
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-24 21:02:37 -0600

    hooks implemented

--- client/src/components/blogs/Blogs.js
+++ client/src/components/blogs/Blogs.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from 'react';
+import HomeTitle from '../styledComponents/homeStyles';
+import axios from 'axios';
 const Blogs = () => {
+  // state = { blogs: [] }
+  const [blogs, setBlogs] = useState([])
+  useEffect( () => {
+    axios.get("/api/blogs")
+      .then( res => {
+        setBlogs(res.data)
+      })
+      .catch( err => console.log(err) )
+  }, [])
+  const addBlog = (blog) => {
+    axios.post("/api/blogs", { blog })
+      .then( res => {
+        // let newBlogs = [...blogs, res.data]
+        // setBlogs(newBlogs)
+        setBlogs([...blogs, res.data])
+      })
+      .catch( err => console.log(err) )
+  }
+  // componentDidMount() {
+  //   axios.get("/api/blogs")
+  //     .then( res => {
+  //       this.setState({ blogs: res.data, });
+  //     })
+  //     .catch( err => {
+  //       console.log(err);
+  //     })
+  // }
   return (
     <>
-      <h1>Blogs Page</h1>
+      <HomeTitle>Blogs Page</HomeTitle>
+      <p>
+        { blogs.map( b => 
+          <>
+            <h1>{b.title}</h1>
+          </>  
+        )}
+      </p>
     </>
   )
 }
--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,4 +1,6 @@
-import { Header, Button, Segment } from 'semantic-ui-react';
+import { Header, Button, Segment, Image } from 'semantic-ui-react';
+import HomeTitle, { HomeSubTitle, MyButton } from '../styledComponents/homeStyles';
+import Food from '../images/food.jpg';
 export const styles = {
   titleHeader: {
     color: "green",
@@ -14,6 +16,7 @@ const Home = () => {
   return (
     <>
       <h1 className="">Home Page</h1>
+      <Image src={Food} tiny />
       <Segment inverted>
         <Header as="h1" color="yellow" textAlign="center" inverted size="large">Home Page</Header>
       </Segment>
@@ -21,6 +24,15 @@ const Home = () => {
       <h1 style={ styles.titleHeader }>Home Page</h1>
       <p style={ styles.pStyles }>hello</p>
       <Button basic color="olive">Click</Button>
+      <MyButton>Click</MyButton>
+      <HomeTitle large>Hello</HomeTitle>
+      <HomeTitle>Hello</HomeTitle>
+      <HomeSubTitle>World</HomeSubTitle>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   )
 }
